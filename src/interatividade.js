36119a977c1c4a4dd8a53b50bd3719047636934c
Gabriel Matheus <gabriel65451@gmail.com>
2024-07-07 19:34:34 -0300

    interatividade com JS

---
+++ src/interatividade.js
@@ -0,0 +1,44 @@
+const card1 = document.querySelector('#card1');
+const card2 = document.querySelector('#card2');
+const card3 = document.querySelector('#card3');
+
+const back = document.querySelector('#btn-voltar');
+const next = document.querySelector('#btn-avancar');
+
+let item = 1;
+const totalIntens = 3;
+
+function visivel(inicio) {
+
+    card1.style.display = 'none';
+    card2.style.display = 'none';
+    card3.style.display = 'none';
+    
+    if (inicio === 1 ) {
+        card1.style.display = 'block';
+    } else if (inicio === 2 ) {
+        card2.style.display = 'block';
+    } else if (inicio === 3 ) {
+        card3.style.display = 'block';
+    }
+   
+}
+
+function voltar() {
+    item--;
+    if (item < 1) {
+        item = totalIntens;
+    }
+    visivel(item);
+}
+
+function proxima() {
+    item++;
+    if (item > totalIntens) {
+        item = 1;
+    }
+    visivel(item);
+}
+
+back.addEventListener('click', voltar);
+next.addEventListener('click', proxima);
