413d350acf7701fc8ab4a1ac67fff8877a6f2670
Gabriel Matheus <gabriel65451@gmail.com>
2024-07-15 20:16:36 -0300

    atualização2
    resolvendo bug do fundo

--- src/interatividade.js
+++ src/interatividade.js
@@ -29,47 +29,41 @@ function visivel(inicio) {
 
 
     if (inicio === 1 ) {
-
         card1.style.display = 'block';
+        if (card1.style.display === 'block') {
         fundo.style.backgroundImage = 'url(../src/img/fundo001-003.jfif)';
         txtHeader.innerHTML = 'Pokémon de Planta';
-        txtHeader.style.color = 'rgb(9, 255, 0)';
-
+        txtHeader.style.color = 'rgb(9, 255, 0)';}
     } else if (inicio === 2 ) {
-
         card2.style.display = 'block';
-        fundo.style.backgroundImage = 'url(../src/img/fundo001-003.jfif)';
-        txtHeader.innerHTML = 'Pokémon de Planta';
-        txtHeader.style.color = 'rgb(9, 255, 0)';
-
+        if (card2.style.display === 'block') {
+            fundo.style.backgroundImage = 'url(../src/img/fundo001-003.jfif)';
+            txtHeader.innerHTML = 'Pokémon de Planta';
+            txtHeader.style.color = 'rgb(9, 255, 0)';}
     } else if (inicio === 3 ) {
-
         card3.style.display = 'block';
-        fundo.style.backgroundImage = 'url(../src/img/fundo001-003.jfif)';
-        txtHeader.innerHTML = 'Pokémon de Planta';
-        txtHeader.style.color = 'rgb(9, 255, 0)';
-
+        if (card3.style.display === 'block') {
+            fundo.style.backgroundImage = 'url(../src/img/fundo001-003.jfif)';
+            txtHeader.innerHTML = 'Pokémon de Planta';
+            txtHeader.style.color = 'rgb(9, 255, 0)';}
     } else if (inicio === 4 ) {
-
         card4.style.display = 'block';
+        if (card4.style.display === 'block'){
         fundo.style.backgroundImage = 'url(../src/img/fundo004-006.jpg)';
         txtHeader.innerHTML = 'Pokémon de Fogo';
-        txtHeader.style.color = 'rgb(248, 98, 11)';
-
+        txtHeader.style.color = 'rgb(248, 98, 11)';}
     } else if (inicio === 5 ) {
-
         card5.style.display = 'block';
-        fundo.style.backgroundImage = 'url(../src/img/fundo004-006.jpg)';
-        txtHeader.innerHTML = 'Pokémon de Fogo';
-        txtHeader.style.color = 'rgb(248, 98, 11)';
-
+        if (card5.style.display === 'block'){
+            fundo.style.backgroundImage = 'url(../src/img/fundo004-006.jpg)';
+            txtHeader.innerHTML = 'Pokémon de Fogo';
+            txtHeader.style.color = 'rgb(248, 98, 11)';}
     } else if (inicio === 6 ) {
-
         card6.style.display = 'block';
-        fundo.style.backgroundImage = 'url(../src/img/fundo004-006.jpg)';
-        txtHeader.innerHTML = 'Pokémon de Fogo';
-        txtHeader.style.color = 'rgb(248, 98, 11)';
-
+        if (card6.style.display === 'block'){
+            fundo.style.backgroundImage = 'url(../src/img/fundo004-006.jpg)';
+            txtHeader.innerHTML = 'Pokémon de Fogo';
+            txtHeader.style.color = 'rgb(248, 98, 11)';}
     }
    
 }
